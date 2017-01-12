3fa70add49676d53a6a0453761bc0bcf66096cbe
baui <bauer.mart>
2017-01-12 20:34:47 +0100

    HTML Version zur Anfrage des Google Kalenders

---
+++ app/src/main/java/eah/eah_stundenplan_app/Main.java
@@ -0,0 +1,16 @@
+package eah.eah_stundenplan_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Martin Bauer on 11.01.2017.
+ */
+
+public class Main extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
